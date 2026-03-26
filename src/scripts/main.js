9ee709852a24a8731196301a8f098c86fdfbf449
Ihor Tutak <igor2tak2212@gmail.com>
2026-03-26 10:03:03 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write your code here
+const population = document.querySelectorAll('.population');
+const populationList = [...population];
+const result = [];
+
+populationList.forEach((element) => {
+  const raw = element.textContent.trim();
+  const cleaned = raw.replace(/\s+/g, '').replace(/,/g, '');
+  const num = Number(cleaned);
+
+  result.push(num);
+});
+
+const total = result.reduce((sum, people) => sum + people, 0);
+const average = Math.round(total / result.length);
+const totalPopulation = document.querySelector('.total-population');
+const averagePopulation = document.querySelector('.average-population');
+const fmt = (v) => new Intl.NumberFormat('en-US').format(v);
+
+totalPopulation.textContent = fmt(total);
+averagePopulation.textContent = fmt(average);
